89e0d6a624929d8d20f42d92981925941bc3e5a6
Jonas Mets <jonas.mets@outlook.com>
2020-09-01 14:36:49 +0200

    some update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
-    
+    <!-- header -->
     <Header1></Header1>
 
     <!-- här visas den view som är vald i router -->
-    <router-view/>
+    <router-view />
+
+    <!-- ta bort div efter test -->
+    <div class="" style="color:blue;text-align:center;  height:200px;">
+      <h3 style="margin-top:100px;">ta bort div i App.vue efter test</h3> 
+    </div>
+
+
+
+    <!-- footer -->
+    <Footer1></Footer1>
 
   </div>
 </template>
@@ -13,35 +23,35 @@
 <script>
 // import Navbar from "@/components/navigation/Navbar";
 import Header1 from "@/components/header/Header1";
+import Footer1 from "@/components/footer/Footer1";
 
 export default {
   components: {
     //
-    Header1
-  }
+    Header1,
+    Footer1
+  },
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
 
 /*  här lägger vi de färger som ska användas */
-:root{
-  --primaryColor:#20D3C2;
-  --primaryColorDarker:#1eafa0;
+:root {
+  --primaryColor: #20d3c2;
+  --primaryColorDarker: #1eafa0;
 
-  --mediumGrey: #8A8A8A;
+  --mediumGrey: #8a8a8a;
   --almostBlack: #333333;
 
-  --inputField: #FBFBFB;
+  --inputField: #fbfbfb;
 
-  --lightPink: #FDF3D7;
-
+  --lightPink: #fdf3d7;
 }
 
-.font-roboto{
-  font-family:'Roboto', sans-serif !important;
+.font-roboto {
+  font-family: "Roboto", sans-serif !important;
   font-size: 16px !important;
 }
 
---
+++ src/components/footer/Footer1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="backg-theme-1">
+      <div class="container"></div>
+    </div>
+
+    <div class="backg-theme-2">
+      <div class="container"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.backg-theme-1 {
+  /*  #0E153D */
+  background-color: #0e153d;
+  border-top: #20d3c2 solid 6px;
+  border-bottom: #080c24 sol;
+  min-height: 100px;
+}
+.backg-theme-2 {
+  /*  #0E153D */
+  background-color: #080c24;
+  min-height: 80px;
+}
+</style>
