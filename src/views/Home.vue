424099f86aa693293f59832d5ad25d9f04c1f1f1
Jonas Mets <jonas.mets@outlook.com>
2020-09-04 19:52:31 +0200

    some more updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-
     <section id="bannerHome">
       <BannerHome1></BannerHome1>
     </section>
@@ -9,9 +8,11 @@
     </section>
 
     <section id="shopByCategory">
-      <ShopByCategory1></ShopByCategory1>
+      <!-- <ShopByCategory1></ShopByCategory1> -->
+      <!-- test -->
+      <ShopByCategory12></ShopByCategory12>
+      <!-- test -->
     </section>
-
   </div>
 </template>
 
@@ -20,14 +21,16 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 import BannerHome1 from "@/components/banner/BannerHome1.vue";
 import UserArgument1 from "@/components/why-use-our-site/UserArgument1.vue";
-import ShopByCategory1 from "@/components/shop-by-category/ShopByCategory1.vue"
+// import ShopByCategory1 from "@/components/shop-by-category/ShopByCategory1.vue";
+import ShopByCategory12 from "@/components/shop-by-category/ShopByCategory1_2.vue";
 
 export default {
   name: "Home",
   components: {
     BannerHome1,
     UserArgument1,
-    ShopByCategory1
+    // ShopByCategory1,
+    ShopByCategory12
   },
 };
 </script>
