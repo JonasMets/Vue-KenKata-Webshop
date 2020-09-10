c87491b1869fab6e1746d570aa8a71dc2bb79377
Jonas Mets <jonas.mets@outlook.com>
2020-09-10 16:20:07 +0200

    an  update this is

--- src/components/latest-blog/LatestBlogCard.vue
+++ src/components/latest-blog/LatestBlogCard.vue
@@ -10,19 +10,31 @@
           <p class="p-0 m-0 my-text-white">MAR</p>
         </div>
 
-        <div class="ml-4">
+        <div class="ml-4 mt-5 pt-2">
           <p class="my-text-white fz-14">
             By
             <span>
               <a href class="my-text-white">Adam Moore</a>
             </span>
           </p>
-          <div class="">
+          <div class>
             <p class="color-primary text-wrap fs-16 font-weight-medium" style="width: 12rem;">
               A beautiful scenery in the
               world we are missing
             </p>
           </div>
+          <!--  -->
+          <div class="d-flex justify-content-between" style="width: 8.5rem;">
+            <button type="button" class="btn btn-sm text-light my-btn-border">Travel</button>
+            <button type="button" class="btn btn-sm text-light my-btn-border">Fashion</button>
+          </div>
+          <div>
+            <button
+              class="btn rounded-pill color-primary btn-readmore text-uppercase pl-4 pr-4 mt-3"
+            >Shop Now</button>
+          </div>
+
+          <!--  -->
         </div>
       </div>
     </div>
@@ -59,4 +71,16 @@ export default {};
   background-color: var(--primaryColor);
   padding: 0;
 }
+
+.my-btn-border {
+  border-color: var(--white) !important;
+}
+
+.btn-readmore {
+  background-color: #ffffff;
+  /* color: var(--); */
+  font-size: 18px;
+  font-weight: 500;
+}
+
 </style>
